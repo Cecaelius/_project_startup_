b364baa3bed47703035b5ad0fd79474b28f1fc25
Ismail Hossain <135722610+Cecaelius@users.noreply.github.com>
2025-08-19 22:34:52 +0600

    Delete _project_startup_ directory

--- _project_startup_/Rotating Navigation/script.js
+++
@@ -1,8 +0,0 @@
-const Search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
-
-btn.addEventListener('click', () => {
-    Search.classList.toggle('active')
-    input.focus()
-})
